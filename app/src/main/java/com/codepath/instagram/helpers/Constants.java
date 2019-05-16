7565d3ee11b626b599fcbdb5d31bd7017f48222e
ramesh kumar <smtcrms@gmail.com>
2019-05-16 17:31:20 +0530

    Add files via upload

---
+++ app/src/main/java/com/codepath/instagram/helpers/Constants.java
@@ -0,0 +1,6 @@
+package com.codepath.instagram.helpers;
+
+public class Constants {
+    public static final String REDIRECT_URI = "oauth://codepath.com";
+    public static final String SCOPE = "basic";
+}
